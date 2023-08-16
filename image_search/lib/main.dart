a7dc82bf6a44d730225614122a73d7edf256a7d6
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-16 17:39:00 +0900

    refactor: add dependency inject from output

--- image_search/lib/main.dart
+++ image_search/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:image_search/data/photo_provider.dart';
 import 'package:image_search/screen/home_screen.dart';
+
+import 'data/pixabayapi.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
+    return MaterialApp(
+        home: PhotoProvider(
+      api: PixabayApi(),
+      child: const HomeScreen(),
+    ));
   }
 }
