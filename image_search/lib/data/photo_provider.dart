3c9bc35ea0beab20c1f435358623f473053108ba
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-16 17:37:08 +0900

    feat: impl data provider

---
+++ image_search/lib/data/photo_provider.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:image_search/data/pixabayapi.dart';
+
+import '../model/Photo.dart';
+
+class PhotoProvider extends InheritedWidget {
+
+  final PixabayApi api;
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
+
+  PhotoProvider({
+    Key? key,
+    required this.api,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  static PhotoProvider of(BuildContext context) {
+    final PhotoProvider? result =
+      context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
+    assert(result != null, 'NO PIXABAYAPI');
+    return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetchValue(query);
+    _photoStreamController.add(result);
+  }
+
+  @override
+  bool updateShouldNotify(PhotoProvider oldWidget) {
+    return oldWidget.api != api;
+  }
+}
