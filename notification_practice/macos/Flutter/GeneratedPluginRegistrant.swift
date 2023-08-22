6955cc6e7824ad6040262bb1c31df488f6766a8e
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-22 10:03:26 +0900

    setting: add build setting file

--- notification_practice/macos/Flutter/GeneratedPluginRegistrant.swift
+++ notification_practice/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_app_badger
+import flutter_local_notifications
+import flutter_native_timezone_updated_gradle
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
